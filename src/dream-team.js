b753b636d790ce9000c944d5f573e2a26a44bfbb
izefka <54077631+izefka@users.noreply.github.com>
2021-03-29 21:05:20 +0300

    fix: new

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
-
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(m) {
+  let t=[];
+  let o="";
+  if(m == null) return false;
+  for (let i=0; i<m.length;i++){
+    if(typeof m[i] == 'string' && isNaN(m[i])){ 
+     m[i]=m[i].trimLeft().toUpperCase();
+    t.push(m[i][0]);}}
+    t.sort();
+    for (let i=0; i<t.length;i++){
+      o+=t[i];
+    }
+    return o;
+  };
